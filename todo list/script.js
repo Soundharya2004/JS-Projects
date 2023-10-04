5e7a31fb94432b270889466648ae6729d516ac9e
Soundharya Rajendran <94343668+Soundharya2004@users.noreply.github.com>
2023-10-05 00:30:32 +0530

    Add files via upload

---
+++ todo list/script.js
@@ -0,0 +1,12 @@
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        const taskList = document.getElementById("taskList");
+        const taskItem = document.createElement("li");
+        taskItem.innerText = taskText;
+        taskList.appendChild(taskItem);
+        taskInput.value = "";
+    }
+}
